a937b8d6354b53adf4fd5563a94ce0df5d779bcf
Zhang Yuan <zhang_yuan@franknetwork.co>
2025-01-17 02:06:19 +0800

    Update config file to allow access to env file. env file should now be in frontend dir

--- frontend/users/next.config.ts
+++ frontend/users/next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 require('dotenv').config({ path: '../.env' });
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+  }
 };
 
 export default nextConfig;
