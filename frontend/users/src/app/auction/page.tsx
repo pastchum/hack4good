72af583bf03af42ef5a23b5b2d3a8a1743e251b7
pastchum <patrick.navin.thomas123@gmail.com>
2025-01-17 22:17:57 +0800

    changes

---
+++ frontend/users/src/app/auction/page.tsx
@@ -0,0 +1,10 @@
+import Header from "../components/Header";
+
+export default function AuctionPage() {
+  return (
+    <div>
+      <Header />
+      <div>To be implemented</div>
+    </div>
+  );
+}
